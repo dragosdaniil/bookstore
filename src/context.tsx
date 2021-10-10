e5b2ae5ef4695214fb108b2c6d16883285ce0d24
Dragos Sandica <dragossandica997@gmail.com>
2021-10-10 11:52:32 +0300

    get methods changes

--- src/context.tsx
+++ src/context.tsx
@@ -2,29 +2,23 @@ import React, { useState, useEffect, useContext, useReducer } from "react";
 import axios from "axios";
 import {bookTypes, Props} from './types/customTypes'
 
-const token = Buffer.from(`${process.env.USER}:${process.env.PASS}`, 'utf8').toString('base64')
+const token = Buffer.from(`${process.env.USER}:${process.env.PASS}`).toString('base64');
 const baseUrl = 'http://localhost:5000/api/v1'
 const LAN_URL = `http://${process.env.LAN_URL}:5000/api/v1`
-
+axios.defaults.headers.common['Authorization'] = token;
 const AppContext = React.createContext<any|null>(null);
 
 const AppProvider:React.FC<Props> = ({children})=>{
     const [books, setBooks] = useState<Array<bookTypes>>([{title:'Title',author:'Author', image_url:'image_url',price:0,genre:'Fantasy'}]);
     const [book, setBook] = useState<bookTypes>({title:'Title',author:'Author', image_url:'image_url',price:0,genre:'Fantasy'});
-    
     const getBook = async (title:string)=>{
-        const response = await axios.get(`${baseUrl}/books/title/${title}`,
-        {   headers:{
-                'Authorization':`Basic ${token}`
-            }   
-        });
+        const response = await axios.get(`${baseUrl}/books/title/${title}`);
         setBook(response.data);
     }
 
     const getBooks = async ()=>{
-        const response = await axios.get(`${baseUrl}/books/`, {headers:{
-            'Authorization':`Basic ${token}`
-        }});
+        console.log(`${process.env.USER}`);
+        const response = await axios.get(`${baseUrl}/books/`);
         setBooks(response.data.Books);
     }
 
